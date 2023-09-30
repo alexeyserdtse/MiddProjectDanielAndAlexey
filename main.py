58d7ba51937d415331ae82e4d2ffdac62a586bc8
Alexey <alexeyserdtse90@gmail.com>
2023-09-30 22:09:35 +0300

    basic code of the project

---
+++ main.py
@@ -0,0 +1,15 @@
+# import libs:
+
+try:
+    import sys
+
+    import pandas as pd
+    import plotly
+except:
+    print('Please install package using PIP!')
+    sys.exit()
+
+
+
+if __name__ == '__main__':
+    print('Hello World!')
